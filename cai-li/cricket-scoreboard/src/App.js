b77f094fb1f1e4e1de290541848e6c28da1efb9b
cai-li <kaley.licai@gmail.com>
2022-06-27 18:24:13 +1000

    Commit

---
+++ cai-li/cricket-scoreboard/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import Scoreboard from "./Components/Scoreboard";
+
+
+function App() {
+  return (
+    <>
+    <Scoreboard />
+    </>
+  );
+}
+
+export default App;
--- cai-li/fancy-florist/src/index.js
+++ cai-li/fancy-florist/src/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Hello from './App.js';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-var person = {
-  personName: "Nick",
-  personAge: 24,
-  favorites: [
-    "capybaras",
-    "Tigers",
-    "Dinosaurs count!"
-  ]
-}
 root.render(
   <React.StrictMode>
-    <Hello name={person.personName} age={person.personAge} animals={person.favorites} />
+    <App />
   </React.StrictMode>
 );
 
